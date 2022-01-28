1213a8508f3374ba6bd18f1e43cc65766ad9b079
takahatayuga <takahata.yuga@ais.cmc.osaka-u.ac.jp>
2022-01-28 13:32:22 +0900

    フレーム読み出し部分を修正

--- compress_container/src/compress.cpp
+++ compress_container/src/compress.cpp
@@ -15,6 +15,9 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+extern "C"{
+    #include <turbojpeg.h>
+}
 using namespace std;
 
 #define FRAME_LENGTH 1920*1080*3
@@ -29,6 +32,13 @@ int main(int argc, char *argv[]){
 
     unsigned char* const shared_memory = reinterpret_cast<unsigned char*>(shmat(seg_id, 0, 0));
 
+    const tjhandle handle = tjInitCompress();
+    if(handle == NULL){
+        const std::string err_msg(tjGetErrorStr());
+        _ml::caution("Failed to init JPEG encoder", err_msg);
+        std::exit(EXIT_FAILURE);
+    }
+
     // 共有メモリの文字を読み取る
     int flag = 0;
     char c;
@@ -42,13 +52,35 @@ int main(int argc, char *argv[]){
         cin.get(c);
         if(c == 'q') flag = 1;
         if(c == 'g') {
-            memcpy(frame_data + buf_index * FRAME_LENGTH, shared_memory, FRAME_LENGTH);
+            memcpy(shared_memory + buf_index * FRAME_LENGTH, frame_data, FRAME_LENGTH);
             cv::Mat video_image(1080, 1920, CV_8UC3, frame_data);
+            unsigned char *jpeg_frame = NULL;
+            unsigned long jpeg_size = 0;
+            const int tj_stat = tjCompress2(handle,
+                                    video_image.data,
+                                    video_image.cols,
+                                    video_image.cols*COLOR_CHANNEL_NUM,
+                                    video_image.rows,
+                                    TJPF_RGB,
+                                    &jpeg_frame,
+                                    &jpeg_size,
+                                    TJSAMP_444,
+                                    80,
+                                    TJFLAG_FASTDCT
+            );
+            if(tj_stat == JPEG_FAILED){
+            const string err_msg(tjGetErrorStr());
+            cout << err_msg << endl;
+            }else{
+            const string jpeg_str(jpeg_frame, jpeg_frame+jpeg_size);
+            //this->send_bufs[id]->push(jpeg_str);
+            }
             //cv::imwrite("received_frame.png", received_frame);
             cout << "get frame." << endl;
             buf_index = (buf_index + 1) % 8;
-             }
+        }
     }
+
 
     // 共有メモリをプロセスから切り離す
     shmdt(shared_memory);
