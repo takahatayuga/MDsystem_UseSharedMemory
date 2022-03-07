0d95bf351fa546fadab407d5102c3b1ec53a85dc
takahatayuga <takahatayuga@localhost.localdomain>
2022-03-07 15:21:24 +0900

    Merge branch 'main' of https://github.com/takahatayuga/MDsystem_UseSharedMemory into main

--- compress_container/src/compress.cpp
+++ compress_container/src/compress.cpp
@@ -15,9 +15,12 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+extern "C"{
+    #include <turbojpeg.h>
+}
 using namespace std;
 
-#define FRAME_LENGTH = 1920 * 1080 * 3;
+#define FRAME_LENGTH 1920*1080*3
 
 int main(int argc, char *argv[]){
     const key_t key = atoi(argv[1]); // shared key
@@ -29,26 +32,44 @@ int main(int argc, char *argv[]){
 
     unsigned char* const shared_memory = reinterpret_cast<unsigned char*>(shmat(seg_id, 0, 0));
 
-    // 共有メモリの文字を読み取る
-    int flag = 0;
-    char c;
-    cout << "If you want to close, please type 'q'" << endl;
-    cout << "If you want to read the shared memory, push enter button." << endl;
+    const tjhandle handle = tjInitCompress();
+    if(handle == NULL){
+        const std::string err_msg(tjGetErrorStr());
+        _ml::caution("Failed to init JPEG encoder", err_msg);
+        std::exit(EXIT_FAILURE);
+    }
 
     unsigned char frame_data[FRAME_LENGTH];
     int buf_index = 0;
+    int count = 0;
 
-    while(flag == 0){
-        cin.get(c);
-        if(c == 'q') flag = 1;
-        if(c == 'g') {
-            memcpy(frame_data + buf_index * FRAME_LENGTH, shared_memory, FRAME_LENGTH);
-            cv::Mat video_image(1080, 1920, CV_8UC3, frame_data);
-            //cv::imwrite("received_frame.png", received_frame);
-            cout << "get frame." << endl;
-            buf_index = (buf_index + 1) % 8;
-             }
+    while(count < 100){
+        memcpy(shared_memory + buf_index * FRAME_LENGTH, frame_data, FRAME_LENGTH);
+        cv::Mat video_image(1080, 1920, CV_8UC3, frame_data);
+        unsigned char *jpeg_frame = NULL;
+        unsigned long jpeg_size = 0;
+        const int tj_stat = tjCompress2(handle,
+                                video_image.data,
+                                video_image.cols,
+                                video_image.cols*COLOR_CHANNEL_NUM,
+                                video_image.rows,
+                                TJPF_RGB,
+                                &jpeg_frame,
+                                &jpeg_size,
+                                TJSAMP_444,
+                                80,
+                                TJFLAG_FASTDCT
+        );
+        if(tj_stat == JPEG_FAILED){
+        const string err_msg(tjGetErrorStr());
+        cout << err_msg << endl;
+        }else{
+        const string jpeg_str(jpeg_frame, jpeg_frame+jpeg_size);
+        }
+        buf_index = (buf_index + 1) % 8;
+        }
     }
+
 
     // 共有メモリをプロセスから切り離す
     shmdt(shared_memory);
